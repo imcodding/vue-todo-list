54d2215e5c87df65927d2c5659e4cf1ff23dee28
sooyun <55366664+imcodding@users.noreply.github.com>
2023-02-08 09:35:10 +0900

    헤더 구현

--- todo-list/src/components/TodoHeader.vue
+++ todo-list/src/components/TodoHeader.vue
@@ -1,15 +1,14 @@
 <template>
-  <div>
-    header
-  </div>
+  <header>
+    <h1>Todo it!</h1>
+  </header>
 </template>
 
-<script>
-export default {
-
+<!-- scoped: 해당 컴포넌트 안에서만 유효한 style로 설정 -->
+<style scoped>
+h1 {
+  color: #2F3B52;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
 }
-</script>
-
-<style>
-
 </style>
