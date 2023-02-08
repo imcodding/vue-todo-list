d4199d32687f365d70028d3cc397c977cca76911
sooyun <55366664+imcodding@users.noreply.github.com>
2023-02-08 14:24:56 +0900

    할 일 목록 조회 기능 구현

--- todo-list/src/components/TodoList.vue
+++ todo-list/src/components/TodoList.vue
@@ -1,15 +1,65 @@
 <template>
   <div>
-    list
+    <ul>
+      <li v-for="todoItem in todoItems" v-bind:key="todoItem" class="shadow">
+        {{todoItem}}
+        <span class="removeBtn">
+          <i class="fas fa-trash-alt"></i>
+        </span>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-
+  data: function() {
+    return {
+      todoItems: []
+    }
+  },
+  //인스턴스가 생성되자마자 호출되는 lifecycle hook
+  created: function() {
+    if(localStorage.length > 0) {
+      for(var i = 0; i < localStorage.length; i++) {
+        this.todoItems.push(localStorage.key(i));
+      }
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+ul {
+  list-style-type: none;
+  padding-left: 0;
+  margin-top: 0;
+  text-align: left;
+}
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+.checkBtnCompleted {
+  color: #b3adad;
+}
+.textCompleted {
+  text-decoration: line-through;
+  color: #b3adad;
+}
+.removeBtn {
+  margin-left: auto;
+  color: #de4343
+}
 </style>
