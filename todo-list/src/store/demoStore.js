35768896d5290f92cc4d225a8b610ac0e10f6bcf
sooyun <55366664+imcodding@users.noreply.github.com>
2023-02-14 09:22:25 +0900

    헬퍼 함수
    - 간단한 예시를 통한 헬퍼 함수의 편리성

---
+++ todo-list/src/store/demoStore.js
@@ -0,0 +1,19 @@
+import Vuex from 'vuex';
+
+export const store = new Vuex.Store({
+    state: {
+        price: 100
+    },
+    getters: {
+        originalPrice(state) {
+            return state.price;
+        },
+        doublePrice(state) {
+            return state.price * 2;
+        },
+        triplePrice(state) {
+            return state.price * 3;
+        }
+    },
+    
+});
